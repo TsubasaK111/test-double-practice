11f8f994e23b33f797e34c3fd7d585658e3b9cd4
Code Chrysalis <codechrysalis@CC-Mac-Mini-4.local>
2018-07-24 12:08:31 +0900

    Feat: spy on fs.readFileSync
    Co-authored-by: Kimiko Motoyama <kimikomotoyama@gmail.com>
    Co-authored-by: Tsubasa Kondo <tsubasak111@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,31 +1,44 @@
 const { expect } = require("chai");
 const Ghibliator = require("../Ghibliator");
+const sinon = require("sinon");
+const fs = require("fs");
 
 describe("Ghibliator", () => {
-  it("should return average age of characters", () => {
-    // setup
-    const expected = 33;
-    const delta = 1;
+  describe("getAverageAge", () => {
+    it("should call fs.readFileSync", () => {
+      const readFileSyncSpy = sinon.spy(fs, "readFileSync");
+      const actual = new Ghibliator().getAverageAge();
+      readFileSyncSpy.restore();
+      sinon.assert.calledOnce(readFileSyncSpy);
+    });
+    it("should return average age of characters", () => {
+      // setup
+      const expected = 33;
+      const delta = 1;
 
-    // exercise
-    const actual = new Ghibliator().getAverageAge();
+      // exercise
+      const actual = new Ghibliator().getAverageAge();
 
-    // assert
-    expect(actual).closeTo(actual, expected, delta);
+      // assert
+      expect(actual).closeTo(actual, expected, delta);
 
-    // teardown
+      // teardown
+    });
   });
+  describe("appearances", () => {
+    it("should return appearances", () => {
+      // setup
+      const expected = 2;
 
-  it("should return appearances", () => {
-    // setup
-    const expected = 2;
+      // exercise
+      const actual = new Ghibliator().appearances(
+        "Baron Humbert von Gikkingen"
+      );
 
-    // exercise
-    const actual = new Ghibliator().appearances("Baron Humbert von Gikkingen");
+      // assert
+      expect(actual).to.equal(actual, expected);
 
-    // assert
-    expect(actual).to.equal(actual, expected);
-
-    // teardown
+      // teardown
+    });
   });
 });
