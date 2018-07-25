825040ec02284e33a49a3899febf5be6cd93b34f
Code Chrysalis <codechrysalis@CC-Mac-Mini-4.local>
2018-07-25 10:59:39 +0900

    feat: reimplement tests for appearances()

--- test/test.js
+++ test/test.js
@@ -4,19 +4,22 @@ const sinon = require("sinon");
 const fs = require("fs");
 
 describe("Ghibliator", () => {
-  describe("getAverageAge", () => {
-    let ghibliator;
-    let readFileSync;
+  let ghibliator;
+  let readFileSync;
 
+  beforeEach(() => {
+    ghibliator = new Ghibliator();
+  });
+
+  afterEach(() => {
+    readFileSync.restore();
+  });
+
+  describe("getAverageAge", () => {
     beforeEach(() => {
-      ghibliator = new Ghibliator();
       readFileSync = sinon.stub(fs, "readFileSync").callsFake(() => {
         return `[{ "age": "17" }, { "age": "late teens" }]`;
       });
-    });
-
-    afterEach(() => {
-      readFileSync.restore();
     });
 
     it("should return a number", () => {
@@ -39,34 +42,47 @@ describe("Ghibliator", () => {
       // setup
       const expected = 33;
       const delta = 1;
-
       // exercise
       const actual = ghibliator.getAverageAge();
-
       // assert
       expect(actual).closeTo(actual, expected, delta);
-
       // teardown
     });
   });
   describe("appearances", () => {
+    beforeEach(() => {
+      readFileSync = sinon.stub(fs, "readFileSync").callsFake(() => {
+        return (
+          `[{ "name": "San", "films": ["a", "b"] }, ` +
+          `{ "name": "Baron Humbert von Gikkingen", "films": ["a", "b"] }]`
+        );
+      });
+    });
+
     it("should return a number", () => {
-      const actual = new Ghibliator().appearances();
+      const actual = new Ghibliator().appearances("San");
       expect(actual).to.be.a("number");
+    });
+
+    it("should call fs.readFileSync", () => {
+      ghibliator.appearances("San");
+      sinon.assert.calledOnce(readFileSync);
+    });
+
+    it("should call readFileSync with correct arguments", () => {
+      ghibliator.appearances("San");
+      sinon.assert.calledWith(readFileSync, "./data.json", "utf-8");
     });
 
     it("should return appearances", () => {
       // setup
       const expected = 2;
-
       // exercise
       const actual = new Ghibliator().appearances(
         "Baron Humbert von Gikkingen"
       );
-
       // assert
       expect(actual).to.equal(actual, expected);
-
       // teardown
     });
   });
